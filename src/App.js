d51b48220053aa1f5830aa659329d8b4a5edf92a
Vikash <vikash6038@gmail.com>
2021-09-06 17:22:48 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
-import logo from './logo.svg';
-import { useEffect, useCallback, useState } from 'react';
-import { Drink, DrinkList } from './drink.js';
-import data from './data.js';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
-  let [myDrinks, setMyDrinks] = useState(data);
-
-  // useEffect(() => {
-
-  // }, []);
-
-  const getDrinks = useCallback(async () => {
-    let resp = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
-      .then(response => response.json())
-      .then(res => {
-        setMyDrinks(res.drinks);
-        debugger;
-      });
-  }, []);
-
   return (
-    <div className="App" >
-      {/* <header className=""> */}
-        <div className="App-header container-fluid">
-          <DrinkList myDrinks={myDrinks} drinkLoader={getDrinks}></DrinkList>
-        </div>
-
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      {/* </header> */}
-    </div>
+    <>
+        <Header />
+    </>
   );
 }
 
--- src/drink.js
+++ src/drink.js
@@ -1,124 +1,68 @@
-import { Component, React, useState } from 'react';
-import Modal from 'react-modal';
+import React, { useState } from "react";
+import Modal from "react-modal";
 
-function Drink(props) {
-    const customStyles = {
-        content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-        },
-    };
-    let [modalIsOpen, setIsOpen] = useState(false);
-    let subtitle;
-    function openModal() {
-        setIsOpen(true);
-    }
+const Drink = ({ id, name, image, title, instruction }) => {
 
-    function afterOpenModal() {
-        // references are now sync'd and can be accessed.
-        // subtitle.style.color = '#f00';
-    }
+  let [modalIsOpen, setIsOpen] = useState(false);
 
-    function closeModal() {
-        setIsOpen(false);
-    }
+  function openModal() {
+    setIsOpen(true);
+  }
 
-    // render() {
-    return (
-        <div id={'drink' + props.id}>
-            <div className="row">
-                <div onClick={openModal} className="col-sm-12">
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <img src={props.image} alt='Drink_image' style={{ width: '100%' }} />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <label>{props.name}</label>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  function closeModal() {
+    setIsOpen(false);
+  }
 
-            <Modal isOpen={modalIsOpen}
-                onAfterOpen={afterOpenModal}
-                onRequestClose={closeModal}
-                style={customStyles}
-                contentLabel="Example Modal"
-            >
-                <div className="container-fluid modalElement">
-                    <div className="row">
-                        <div className="col-sm-6">
-                            <img src={props.image} alt='Drink_image' style={{ width: '100%' }} />
+  return (
+    <div>
 
-                        </div>
-                        <div className="col-sm-6">
-                            <div className="row">
-                                <div className="col-sm-12">
-                                    <b>Instructions</b>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-sm-12">
-                                    <label>
-                                        {props.instructions}
-                                    </label>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-sm-12">
-                                    <b>Glass</b>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-sm-12">
-                                    Serve:{props.glass}
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-
-                <button onClick={closeModal}>close</button>
-
-            </Modal>
-        </div >
-    );
-    // }
-}
-
-function DrinkList(props) {
-    return (
-        <div >
-            <div className="row">
-                <div className="col-sm-5 col-*-offset-4">
-                    <input type="text" id="search" />
-                    <button type="button" className="fa fa-search" onClick={() => { props.drinkLoader() }}>Search</button>
-                </div>
-            </div>
-            {props.myDrinks.length ?
-                (<div className="row">
-                    {props.myDrinks.map((drk, idx) => {
-                        return (
-                            <div key={idx} className="col-sm-4">
-                                <Drink id={idx}
-                                    image={drk.strDrinkThumb}
-                                    name={drk.strDrink}
-                                    glass={drk.strGlass}
-                                    instructions={drk.strInstructions}>
-                                </Drink>
-                            </div>)
-                    })}
-                </div>)
-                : (<div>Loading drinks...</div>)
-            }
+      <div onClick={openModal}>
+        <div className="img-div">
+          <img src={image} alt="Loading..." />
         </div>
-    )
-}
-export { Drink, DrinkList };
+        <h6 className="heading">{name}</h6>
+      </div>
+
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+
+        <div className="container modalElement">
+         
+          <div className="row p-5">
+            <div className="col-sm-6 modalimg">
+            <div className="row">
+                <div className="col-sm-12">{title}</div>
+              </div>
+              <img src={image} alt="Drink_image"/>
+            </div>
+            <div className="col-sm-6 p-5">
+              <div className="row p-2">
+                <div className="col-sm-12">
+                  <b>Instructions</b>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-12">
+                  <label>{instruction}</label>
+                </div>
+              </div>
+              <div className="row p-3">
+                <div className="col-sm-12">
+                  <b>Glass</b>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-12">Serve : {title}</div>
+              </div>
+            </div>
+            <div>
+        <button className='closebtn p-2' onClick={closeModal}>X</button>
+        </div>
+          </div>
+        </div>
+
+      </Modal>
+    </div>
+  );
+};
+
+export default Drink;
